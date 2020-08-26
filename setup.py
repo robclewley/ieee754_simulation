a13212a66d3813c9e436acd8bce2120dcd6f482a
Rob Clewley <4868709+robclewley@users.noreply.github.com>
2020-08-26 01:30:28 -0400

    Added URL and other metadata

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='Simfloat',
@@ -8,4 +8,15 @@ setup(
     packages=['simfloat',],
     license='BSD 2-Clause',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+            "License :: OSI Approved :: BSD License",
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+            "Development Status :: 5 - Production/Stable",
+            "Topic :: Education",
+            "Topic :: Scientific/Engineering"
+        ],
+    url="https://github.com/robclewley/ieee754_simulation",
+    install_requires=['numpy']
 )
